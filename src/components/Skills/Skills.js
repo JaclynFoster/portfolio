2499bc56f7e0db4b27f6a78d527c7ce26b812561
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-05-31 17:26:57 -0500

    added skills section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,28 +8,49 @@ import jsLogo from '../Skills/logos/javascript-logo.png'
 import nodeLogo from '../Skills/logos/node-logo.png'
 import pgLogo from '../Skills/logos/postgres-logo.png'
 import reactLogo from '../Skills/logos/react-logo.png'
+import bitLogo from '../Skills/logos/bitio-logo.png'
+import routerLogo from '../Skills/logos/router-logo.png'
+import sequelizeLogo from '../Skills/logos/sequelize-logo.png'
 import '../Skills/Skills.css'
 
 
 const Skills = () => {
     return (
       <Layout>
-        <div>
+        <div className="skill-container">
             <h3>My Skillset includes the following:</h3>
-            <div>
-               <img className="logo" src={axiosLogo}/>
-               <img className="logo" src={cssLogo}/>
-               <img className="logo" src={expressLogo}/>
+                <h3>Front-End:</h3>    
+            <div className="logo-container">
+               <label>HTML</label>
                <img className="logo" src={htmlLogo}/>
+               <label>CSS</label>
+               <img className="logo" src={cssLogo}/>
+               <label>Javascript</label>
                <img className="logo" src={jsLogo}/>
-               <img className="logo" src={nodeLogo}/>
+               <label>React</label>
                <img className="logo" src={reactLogo}/>
+               <label>React Router</label>
+               <img className="logo router" src={routerLogo}/>
+            </div>
+               <h3>Back-End:</h3>
+            <div className="logo-container">
+               <label>Axios</label>
+               <img className="logo" src={axiosLogo}/>
+               <label>Express</label>
+               <img className="logo" src={expressLogo}/>
+               <label>Node</label>
+               <img className="logo" src={nodeLogo}/>
+               <label>Sequelize</label>
+               <img className="logo" src={sequelizeLogo}/>
+               <label>PostgreSQL</label>
                <img className="logo" src={pgLogo}/>
+               <label>Bit.io</label>
+               <img className="logo" src={bitLogo}/>
             </div>
 
         </div>
-        <div>
-            <h3>Bonus Knowledge:</h3>
+        <div className="bonus-container">
+            <h4>Bonus Knowledge:</h4>
             <ul>
                 <li>10+ years experience in the medical field</li>
                 <li>7+ years experience in management</li>
