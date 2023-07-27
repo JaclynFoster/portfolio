c3ae61862eb5cc0516153d5c981b4a8182562af9
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-07-27 11:51:01 -0500

    completed projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,91 +1,139 @@
 import React from 'react'
 import Layout from '../Layout/Layout.js'
-import crochet from './crochet-app.png'
 import { Card, Divider } from 'antd'
 import '../Projects/Projects.css'
 
 const Projects = () => {
   return (
     <Layout>
-      <Card className="crochet-card" title="Crochet Pattern App">
-        <iframe
-          width="360"
-          height="215"
-          src="https://www.youtube.com/embed/4y6BUayiu8Q"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        />
-        <Divider />
-        <p>
-          This app allows you to see pictures of crochet projects, view their
-          price, and visit the pattern link. You can create a new pattern card,
-          update a current pattern card, or delete a card. This is a full CRUD
-          application.
-        </p>
-        <label>Front End:</label>
-        <p>Javascript, HTML, CSS, Axios</p>
-        <label>Back End:</label>
-        <p>Node, Express, Cors</p>
-        <a href="https://github.com/JaclynFoster/full-crud-app" target="_blank">
-          Github Link
-        </a>
-      </Card>
-      <Card className="neighbor-card" title="Neighborhood Chat Page">
-        <iframe
-          width="360"
-          height="215"
-          src="https://www.youtube.com/embed/DrIzf3Ld2RE"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        />
-        <Divider />
-        <p>
-          This is a full CRUD application hosted through AWS and Postgres
-          database through Bit.io. Users can view their profile information on
-          their profile card, create posts, like posts, add comments to other
-          user's posts and delete their own posts.
-        </p>
-        <label>Front End:</label>
-        <p>Javascript, HTML, CSS, React, React Router, Context API, Axios</p>
-        <label>Back End:</label>
-        <p>Node, Express, Cors, Sequelize</p>
-        <a
-          href="https://github.com/JaclynFoster/neighborhood-chat-page"
-          target="_blank"
-        >
-          Github Link
-        </a>
-        <a className="site" href="http://54.177.82.121:3400/" target="_blank">
-          Site Link
-        </a>
-      </Card>
-      <Card className="neighbor-card" title="E-Commerce Site">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/0IOMiHQT6Gg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <Divider />
-        <p>
-          This is a full-stack E-Commerce site allowing users to browse animals online, add to cart and checkout. The user can signin or create an account and can only view certain pages when logged in. Hosted through serverless S3 via AWS and database is through PostgreSQL Cockroach DB.
-        </p>
-        <label>Front End:</label>
-        <p>Javascript, HTML, CSS, React, React Router, Redux, Context API, Axios, Bootstrap</p>
-        <label>Back End:</label>
-        <p>Node, Express, Cors, PostgreSQL, Bcrypt, JWT</p>
-        <a
-          href="https://github.com/JaclynFoster/store-app"
-          target="_blank"
-        >
-          Github Link
-        </a>
-        <a className="site" href="fawsturfarms.jaclyndfoster.com" target="_blank">
-          Site Link
-        </a>
-      </Card>
+      <div className="card-container">
+        <Card className="store-card" title="Crochet Pattern App">
+          <div className="video">
+            <iframe
+              src="https://www.youtube.com/embed/4y6BUayiu8Q"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            />
+          </div>
+          <Divider />
+          <div>
+            <p className="cardP">
+              This app allows you to see pictures of crochet projects, view
+              their price, and visit the pattern link. You can create a new
+              pattern card, update a current pattern card, or delete a card.
+              This is a full CRUD application.
+            </p>
+            <label>Front End:</label>
+            <p>Javascript, HTML, CSS, Axios</p>
+            <label>Back End:</label>
+            <p>Node, Express, Cors</p>
+          </div>
+          <div className="card-link">
+            <a
+              href="https://github.com/JaclynFoster/full-crud-app"
+              target="_blank"
+            >
+              Github Link
+            </a>
+          </div>
+        </Card>
+        <Card className="store-card" title="Neighborhood Chat Page">
+          <div className="video">
+            <iframe
+              src="https://www.youtube.com/embed/DrIzf3Ld2RE"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            />
+          </div>
+          <Divider />
+          <div className="cardP">
+            <p>
+              This is a full CRUD application hosted through AWS and Postgres
+              database through Bit.io. Users can view their profile information
+              on their profile card, create posts, like posts, add comments to
+              other user's posts and delete their own posts.
+            </p>
+            <label>Front End:</label>
+            <p>
+              Javascript, HTML, CSS, React, React Router, Context API, Axios
+            </p>
+            <label>Back End:</label>
+            <p>Node, Express, Cors, Sequelize</p>
+          </div>
+          <div className="card-link">
+            <a
+              href="https://github.com/JaclynFoster/neighborhood-chat-page"
+              target="_blank"
+            >
+              Github Link
+            </a>
+            <Divider type="vertical" />
+            <a
+              className="site"
+              href="http://54.177.82.121:3400/"
+              target="_blank"
+            >
+              Site Link
+            </a>
+          </div>
+        </Card>
+        <Card className="store-card" title="E-Commerce Site">
+          <div className="card-flex">
+            <div className="video">
+              <iframe
+                src="https://www.youtube.com/embed/0IOMiHQT6Gg"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              />
+            </div>
+            <Divider />
+            <div className="cardP">
+              <p>
+                This is a full-stack E-Commerce site allowing users to browse
+                animals online, add to cart and checkout. The user can signin or
+                create an account and can only view certain pages when logged
+                in. Hosted through serverless S3 via AWS and database is through
+                PostgreSQL Cockroach DB.
+              </p>
+              <label>Front End:</label>
+              <p>
+                Javascript, HTML, CSS, React, React Router, Redux, Context API,
+                Axios, Bootstrap
+              </p>
+              <label>Back End:</label>
+              <p>Node, Express, Cors, PostgreSQL, Bcrypt, JWT</p>
+            </div>
+            <div className="card-link">
+              <a
+                href="https://github.com/JaclynFoster/store-app"
+                target="_blank"
+              >
+                Github Link
+              </a>
+              <Divider type="vertical" />
+              <a
+                className="site"
+                href="fawsturfarms.jaclyndfoster.com"
+                target="_blank"
+              >
+                Site Link
+              </a>
+            </div>
+          </div>
+        </Card>
+      </div>
     </Layout>
   )
 }
 
 export default Projects
+
+
+
 
