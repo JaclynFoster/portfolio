9566f1634ca41e923da4ab64cef62c036d2c413d
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-09-05 17:08:25 -0500

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -3,13 +3,7 @@ const cors = require('cors')
 const { json } = require('body-parser')
 require('dotenv').config()
 
-const send = require('gmail-send')({
-  user: process.env.username,
-  password: process.env.password,
-  to: 'shahanjaclyn@gmail.com'
-})
-
-const port = 3000
+const port = process.env.SERVER_PORT
 
 const app = express()
 
@@ -17,23 +11,10 @@ app.use(express.static(`${__dirname}/../public`))
 app.use(json())
 app.use(cors())
 
-app.post('/api/sendEmail', (req, res) => {
-  send(
-    {
-      subject: 'JOB PROSPECT FROM: ' + req.body.name + ' at ' + req.body.email,
-      text: `Name: ${req.body.name}\nEmail: ${req.body.email}\nMessage: ${
-        req.body.message
-      }`
-    },
-    function (err, res) {
-      console.log(' * ERROR send() callback returned: err', err, '; res:', res)
-    }
-  )
-  console.log('working')
-  res.status(200).json((message = 'working'))
-})
+app.post('/mail/sendMail', sendMail)
 
 const path = require('path')
+const { sendMail } = require('./controllers/GmailController')
 app.get('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '/../public/index.html'))
 })
